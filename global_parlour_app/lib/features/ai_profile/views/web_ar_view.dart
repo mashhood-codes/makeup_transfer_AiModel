01659ad91fc65e54b5d16d273b35c13cc871e6eb
mashhood-codes <233807@students.au.edu.pk>
2026-04-14 01:45:56 +0500

    Clean: Remove Flutter app files from repo, keep only makeup_transfer_workspace for hosting

--- global_parlour_app/lib/features/ai_profile/views/web_ar_view.dart
+++
@@ -1,2 +0,0 @@
-export 'web_ar_view_stub.dart'
-  if (dart.library.html) 'web_ar_view_web.dart';
--- glowup_ai_ar/android/app/src/main/kotlin/com/glowup/ar/glowup_ai_ar/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.glowup.ar.glowup_ai_ar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
